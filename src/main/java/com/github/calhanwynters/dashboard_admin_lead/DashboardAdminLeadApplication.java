668a8717ebbfcaddaea33e800a3334042147ad0d
Calhan Wynters <github.calhan@store-fern.8shield.net>
2025-12-21 17:38:00 -0600

    First commit

---
+++ src/main/java/com/github/calhanwynters/dashboard_admin_lead/DashboardAdminLeadApplication.java
@@ -0,0 +1,13 @@
+package com.github.calhanwynters.dashboard_admin_lead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardAdminLeadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardAdminLeadApplication.class, args);
+	}
+
+}
